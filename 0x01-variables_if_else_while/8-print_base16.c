11c7e32af55892faa0471e5ba647a3a849816734
gehad-nageh <gehadgege7654@gmail.com>
2023-09-11 16:11:48 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Descrption: A C program that prints with print function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
