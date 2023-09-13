096eec6c284fac98d7ceadad023d00dda331bccf
gehad-nageh <gehadgege7654@gmail.com>
2023-09-12 23:59:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include"stdio.h"
+/**
+ * main - Entry point
+ *
+ * Descrption: A C program that prints with print function
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_alphabet(void);
+{
+	int ch;
+
+	for (ch ='a'; ch <= 'z';ch++)
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
