08deb640c3890691824541c28b0fcd3b74069884
gehad-nageh <gehadgege7654@gmail.com>
2023-09-18 18:46:56 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - revese a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (1 = 0; s[1] != '\0'; ++i)
+		;
+	for (i = 0; i < 1 / 2; i++)
+	{
+		trmp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+}
