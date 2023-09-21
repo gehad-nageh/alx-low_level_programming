27e4b72c5e2fc120d8bdf2fd7079be1d6e755573
gehad-nageh <gehadgege7654@gmail.com>
2023-09-21 15:44:48 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int c.c2;
+	int c, c2;
 	c = 0;
 
 	while (dest[c])
 		c++;
 	
 	for (c2 = 0; src[c2] ; c2++)
+		
 		dest[c++] = src[c2];
+
 	return (dest);
 }
