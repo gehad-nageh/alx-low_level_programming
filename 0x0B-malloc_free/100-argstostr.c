db23e49470e7e0f51c8f53f44d0c8d2eaab93bc5
gehad-nageh <gehadgege7654@gmail.com>
2023-10-05 15:24:22 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strlen - function to find the length of the string
+ * @s: input string
+ * Return: Nothing
+ */
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+	;
+	return (size);
+}
+
+/**
+ * *argstostr - function that concatenates all the arguments of your program
+ * @ac: input int
+ * @av: input char
+ * Return: Nothing
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i = 0, k = 0, j = 0, c = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (; i < k; i++, k++)
+		k += _strlen(av[i]);
+
+	s = malloc(sizeof(char) * k + 1);
+	if (s == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, c++)
+			s[c] = av[i][j];
+
+		s[c] = '\n';
+		c++;
+	}
+	s[c] = '\0';
+	return (s);
+
+}
