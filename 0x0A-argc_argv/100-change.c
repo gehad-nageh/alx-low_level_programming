e01b78164813a38364e36748f3b574de0f434504
gehad-nageh <gehadgege7654@gmail.com>
2023-10-07 12:59:55 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
 {
 	if (argc == 2)
 	{
-		int i, l = 0, budget = atoi(argv[1]);
-		int c[] = {25, 10, 5, 2, 1}
+	int i, l = 0, budget = atoi(argv[1]);
+	int c[] = {25, 10, 5, 2, 1};
 
 	for (i = 0; i < 5; i++)
 	{
