8527bdc326800be34e5f345472f17e35d540ddfd
gehad-nageh <gehadgege7654@gmail.com>
2023-10-07 09:44:33 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program name
+ * @argc: input int
+ * @argv: inut char
+ * Return: Nothing
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s/n", argv[0]);
+	return (0);
+}
