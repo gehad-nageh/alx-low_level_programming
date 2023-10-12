cb1e35c71b7c9736eab598af7af0e5df12ade321
gehad-nageh <gehadgege7654@gmail.com>
2023-10-11 19:36:28 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,8 +4,8 @@
  * int_index - function that searches for an integer
  * @array: input int
  * @size: size of array
- * @cmp: replace function
- * Return: Nthing
+ * @cmp: compare function
+ * Return: Nothing
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array && size && cmp)
 		while (x < size)
 		{
-			if (cmp(array[i]))
+			if (cmp(array[x]))
 				return (x);
 			x++;
 		}
