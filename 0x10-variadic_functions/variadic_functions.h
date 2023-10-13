b5e0110da84d2f28ecc04479254dc0ce78ddcb46
gehad-nageh <gehadgege7654@gmail.com>
2023-10-12 19:28:09 -0700

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct token - strucr token
+ * @token: format token
+ * @s: the function assoctiated
+ */
+typedef struct token
+{
+	char *token;
+	void (*s)(char *, va_list);
+} token_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
