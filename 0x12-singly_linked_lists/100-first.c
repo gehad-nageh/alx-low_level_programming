3111232eedd4137f8815bbb574f56d450ff733f0
gehad <gehadgege7654@gmail.com>
2023-10-19 19:52:40 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 
-void _constructor(void) __artribute__ ((constructor));
+void _constructor(void) __attribute__ ((constructor));
 
 /**
- * _constructor - function that prints a sentences
- * 
+ * _constructor - function that prints sentences
+ *
  * Return: Nothing
  */
 void _constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
-		"I bore my house upon my back!\n");
-			
+		"I bore my house upon my back!\n")
 }
