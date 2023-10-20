1018f1268a95d1400159a24782dd50b27b54e69f
gehad <gehadgege7654@gmail.com>
2023-10-19 19:47:30 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+void _constructor(void) __artribute__ ((constructor));
+
+/**
+ * _constructor - function that prints a sentences
+ * 
+ * Return: Nothing
+ */
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+			
+}
