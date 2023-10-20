9b27e9dd8b8857a2a97bdcf42772109be391d2d3
gehad <gehadgege7654@gmail.com>
2023-10-19 19:23:17 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,7 +8,7 @@
  * Return: Nothing
  */
 
-list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 	list_t *node = *head;
