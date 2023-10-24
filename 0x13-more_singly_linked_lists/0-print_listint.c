0de4d0fcab95d9f9ea549a6721eae5a0ce08ff5c
gehad <gehadgege7654@gmail.com>
2023-10-24 16:05:50 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements
+ * @h: first pointer
+ * Return: Nothing
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t k = 0;
+	
+	while(h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		k++;
+	}
+	return (k);
+}
