313a3a3044edf9ea048690feb8f8e2442a3ac76b
gehad <gehadgege7654@gmail.com>
2023-10-23 19:36:12 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  * @owner: the name of the dog's owner
  * Return: void
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
