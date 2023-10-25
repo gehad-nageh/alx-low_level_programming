a82e07a6564417470c9c5d2784444bebf9c84c22
gehad <gehadgege7654@gmail.com>
2023-10-24 17:38:55 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that deletes the head node
+ * @head: address of the first point
+ * Return: Nothing
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int k;
+
+	if (!head || !*head)
+		return (0);
+
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
