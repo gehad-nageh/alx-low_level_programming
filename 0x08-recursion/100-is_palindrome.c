43a42522993aea587bcfc8dd95ee6b40c7bca832
gehad <gehadgege7654@gmail.com>
2023-10-25 13:42:50 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * last_index - func that returns the last index of the string
+ * @s: pointer the string
+ * Return: Nothing
+ */
+int is_palindrome(char *s);
+int check(char *s, int start, int end, int mod);
+int last_index(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+		n += last_index(s + 1) + 1;
+	return (n);
+}
+
+/**
+ * is_palindrome - function that check if the string is palindrome
+ * @s: string
+ * Return: Nothing
+ */
+int is_palindrome(char *s)
+{
+	int end = last_index(s);
+
+	return (check(s, 0, end - 1, end % 0));
+}
+
+/**
+ * check - function that check the palindrome
+ * @s: int
+ * @start: int
+ * @end: int
+ * @mod: int
+ * Return: Nothing
+ */
+int check(char *s, int start, int end, int mod)
+{
+	if ((start == end && mod != 0) || (start == end + 1 && mod == 0))
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (check(s, start + 1, end - 1, mod));
+}
