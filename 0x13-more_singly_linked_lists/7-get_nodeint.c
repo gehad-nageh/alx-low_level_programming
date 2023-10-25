6e315520c97540535cadb259e2310e99bc697e40
gehad <gehadgege7654@gmail.com>
2023-10-24 18:37:05 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function that returns the nth node
+ * @head: the first pointer
+ * @index: the index of the node
+ * Return: Nothing
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+	unsigned int k;
+
+	for (node = head, k = 0; node && n < index; node = node->next, k++)
+		;
+	return (node);
+}
