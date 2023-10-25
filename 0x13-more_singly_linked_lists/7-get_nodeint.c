f072d8aeed005afd3c62d61e37f16f063713ec41
gehad <gehadgege7654@gmail.com>
2023-10-24 18:40:11 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *node;
 	unsigned int k;
 
-	for (node = head, k = 0; node && n < index; node = node->next, k++)
+	for (node = head, k = 0; node && k < index; node = node->next, k++)
 		;
 	return (node);
 }
