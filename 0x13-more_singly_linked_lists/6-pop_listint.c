2fd3d2bdb71c839a43765119a96068be9fb942a7
gehad <gehadgege7654@gmail.com>
2023-10-24 17:45:43 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *node;
-	int k;
+	int n;
 
 	if (!head || !*head)
 		return (0);
 
 	node = (*head)->next;
-	k = (*head)->k;
+	n = (*head)->n;
 	free(*head);
 	*head = node;
-	return (k);
+	return (n);
 }
