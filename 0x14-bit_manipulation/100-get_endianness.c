334e49e8933e55756c5ded1e76fc26f6e26c0b17
gehad <gehadgege7654@gmail.com>
2023-10-26 19:58:28 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness
+ * Return: Nothing
+ */
+
+int get_endianness(void)
+{
+	unsigned long int i = 1;
+
+	return (*(char *)&n);
+}
