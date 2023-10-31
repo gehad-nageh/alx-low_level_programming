3b583870e231b7810661fc961d82d900d36627e8
gehad <gehadgege7654@gmail.com>
2023-10-30 18:58:21 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints
+ * it to the POSIX standard output
+ * @filename: name of file
+ * @letters: number of bytes
+ * Return: Nothing
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int i;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	i = open(filename, o_RDONLY);
+	if (i == -1)
+		return (0);
+	bytes = read(i, &buf[0], letters);
+	bytes = write(STDOUT_FILEND, &buf[0], bytes);
+	close(i);
+	return (bytes);
+}
